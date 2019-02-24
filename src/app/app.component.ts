c293730ae51832c3a5b580409821436db7e55bd8
Rufat Gulabli <gulabli.rufat@gmail.com>
2019-02-24 22:45:22 +0400

    star & app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,18 @@ import { EventArgs } from "./star/star.component";
 })
 export class AppComponent {
   title = "Rufat Gulabli";
+  viewMode: string = "other";
   post = {
     isFavorite: true,
     count: 10
+  };
+  private courses;
+  private student = {
+    name: "Rufat",
+    surname: "Gulabli",
+    address: {
+      city: "Baku"
+    }
   };
 
   onChange(eventArgs: EventArgs) {
@@ -22,5 +31,26 @@ export class AppComponent {
     this.post.isFavorite = args.isFavorite;
     console.log(this.post.count);
     console.log(this.post.isFavorite);
+  }
+
+  load() {
+    this.courses = [
+      { id: 1, name: "JavaScript Fundamentals" },
+      { id: 2, name: "Advanced JavaScript" },
+      { id: 3, name: "NodeJS Beginner to Guru" },
+      { id: 4, name: "ExpressJS" },
+      { id: 5, name: "HTML5/CSS3/Bootstrap4" }
+    ];
+  }
+
+  trackByID(index, course) {
+    return course ? course.id : undefined;
+  }
+
+  onDelete(item) {
+    console.log(item);
+    let courseId = this.courses.indexOf(item);
+    console.log(courseId);
+    this.courses.splice(courseId, 1);
   }
 }
