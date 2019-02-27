812f507e010bc47ceba54e89405578332c4e8067
RufatGulabli <gulabli.rufat@gmail.com>
2019-02-27 14:30:55 +0400

    checkbox, select

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,10 +6,14 @@ import { Component } from "@angular/core";
   styleUrls: ["./contact-form.component.css"]
 })
 export class ContactFormComponent {
+  contactMethods = [{ id: 1, name: "Email" }, { id: 2, name: "Phone" }];
   log(val) {
     console.log(val);
   }
   logger(val) {
     console.log(val);
+  }
+  submit(f) {
+    console.log(f);
   }
 }
