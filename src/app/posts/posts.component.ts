f5ec4937fbd2025a1bcd20e518b4bcb8e132f137
RufatGulabli <gulabli.rufat@gmail.com>
2019-03-04 18:07:14 +0400

    Http Call

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
+import { ReplaceSource } from "webpack-sources";
 
 @Component({
   selector: "posts",
@@ -7,15 +8,24 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./posts.component.css"]
 })
 export class PostsComponent implements OnInit {
-  private posts;
-
+  private posts: [];
+  private url: string = "https://jsonplaceholder.typicode.com/posts";
   constructor(private http: HttpClient) {
-    http
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .subscribe(responce => {
-        this.posts = responce;
-      });
+    http.get(this.url).subscribe(responce => {
+      this.posts = responce;
+    });
   }
 
   ngOnInit() {}
+
+  addTodo(newTodo: HTMLInputElement) {
+    let post = { title: newTodo.value };
+    newTodo.value = "";
+    this.http.post(this.url, JSON.stringify(post)).subscribe(response => {
+      console.log(response);
+      post["id"] = JSON.parse(response).id;
+      // this.posts.splice(0, 0, post);
+      console.log(post);
+    });
+  }
 }
